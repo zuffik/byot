f2cf15ca5598829421ba0699f18f09866a6b3997
Kristián Žuffa <kristian.zuffa@gmail.com>
2020-05-03 14:16:48 +0200

    created config.ts

---
+++ common/config.ts
@@ -0,0 +1,12 @@
+export default {
+  info: {
+    name: 'BYOt',
+    description: 'BYOt - Build Your Own Training',
+  },
+  theme: {
+    colors: {
+      primary: '#673AB7',
+      secondary: '#03A9F4',
+    },
+  },
+};

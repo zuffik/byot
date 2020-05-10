9fb400038532f05fea3b131740fad99731324720
Kristián Žuffa <kristian.zuffa@gmail.com>
2020-05-10 21:59:42 +0200

    ci update

--- scripts/logo-favicon-injector.sh
+++ scripts/logo-favicon-injector.sh
@@ -2,6 +2,6 @@
 
 PUBLIC_PATH="../$1/public/"
 
-cp -r out/favicon "$PUBLIC_PATH"
+cp -r out/favicon/* "$PUBLIC_PATH"
 npx real-favicon inject out/favicon-data.json out/favicon "$PUBLIC_PATH/index.html"
 mv out/favicon/index.html "$PUBLIC_PATH"

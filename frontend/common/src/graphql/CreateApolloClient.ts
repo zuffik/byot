c092e5ffc8b8a8d54faf5a7e4ac626ce3937dec5
Kristián Žuffa <kristian.zuffa@gmail.com>
2020-05-13 20:30:43 +0200

    apollo graphql

---
+++ frontend/common/src/graphql/CreateApolloClient.ts
@@ -0,0 +1,6 @@
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
+
+export const createApolloClient = (uri: string): ApolloClient<InMemoryCache> =>
+  new ApolloClient<InMemoryCache>({
+    uri,
+  });

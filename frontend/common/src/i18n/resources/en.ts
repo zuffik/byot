0796bda2592d54d3b1ca95cdfd7f925e7a8e6fd3
Kristián Žuffa <kristian.zuffa@gmail.com>
2020-06-20 16:04:33 +0200

    Updated translations

--- frontend/common/src/i18n/resources/en.ts
+++ frontend/common/src/i18n/resources/en.ts
@@ -6,4 +6,18 @@ export default {
   'Password': 'Password',
   'Forgot password?': 'Forgot password?',
   "Don't have an account?": "Don't have an account?",
+  'Sign up': 'Sign up',
+  'Enter valid email': 'Enter valid email',
+  'Enter email': 'Enter email',
+  'Password must be longer than 8 characters': 'Password must be longer than 8 characters',
+  'Password must contain at least one special character':
+    'Password must contain at least one special character',
+  'Confirm password': 'Confirm password',
+  'Passwords are not the same': 'Passwords are not the same',
+  'You have to agree with our terms and conditions': 'You have to agree with our terms and conditions',
+  'First name': 'First name',
+  'Last name': 'Last name',
+  'Email': 'Email',
+  'I allow this website to collect and store submitted data.':
+    'I allow this website to collect and store submitted data.',
 };

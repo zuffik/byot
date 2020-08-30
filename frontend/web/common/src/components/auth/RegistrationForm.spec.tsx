7780932ea664096975ff71e310d4865ac446a7cc
Kristián Žuffa <kristian.zuffa@gmail.com>
2020-08-30 16:41:51 +0200

    unit frontend web common tests

--- frontend/web/common/src/components/auth/RegistrationForm.spec.tsx
+++ frontend/web/common/src/components/auth/RegistrationForm.spec.tsx
@@ -47,7 +47,7 @@ describe('<RegistrationForm/>', () => {
       firstName: 'John',
       lastName: 'Doe',
       password: '123-Abc-',
-      userName: '',
+      userName: undefined,
     };
     const data = {
       ...registrationData,
@@ -71,7 +71,7 @@ describe('<RegistrationForm/>', () => {
       firstName: '',
       lastName: '',
       password: '123-Abc-',
-      userName: '',
+      userName: undefined,
     };
     const data = {
       ...registrationData,
@@ -95,7 +95,7 @@ describe('<RegistrationForm/>', () => {
       firstName: 'John',
       lastName: 'Doe',
       password: '123-Abc-',
-      userName: '',
+      userName: undefined,
     };
     const data = {
       ...registrationData,
@@ -119,7 +119,7 @@ describe('<RegistrationForm/>', () => {
       firstName: '',
       lastName: '',
       password: '',
-      userName: '',
+      userName: undefined,
     };
     const data = {
       ...registrationData,
@@ -143,7 +143,7 @@ describe('<RegistrationForm/>', () => {
       firstName: '',
       lastName: '',
       password: 'weak',
-      userName: '',
+      userName: undefined,
     };
     const data = {
       ...registrationData,
@@ -167,7 +167,7 @@ describe('<RegistrationForm/>', () => {
       firstName: '',
       lastName: '',
       password: '123-ABc-',
-      userName: '',
+      userName: undefined,
     };
     const data = {
       ...registrationData,
